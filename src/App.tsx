b58132f9905039dad99ab577c0490568ac52991f
rawanelkhodary <relkhodary@smart.sa>
2025-04-12 02:40:15 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Card from './components/card/Card'
+import Filter from './components/filter/Filter'
+import Header from './components/header/Header'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Header/>
+      <Filter/>
+      <Card/>
+    </>
+  )
+}
+
+export default App
