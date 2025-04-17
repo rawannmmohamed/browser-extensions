08e20f0345fd9515c46a626a62957e0d1684566f
rawanelkhodary <relkhodary@smart.sa>
2025-04-18 01:04:43 +0200

    `Added theme switching functionality to the application`

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
-import './App.css'
-import Card from './components/card/Card'
-import Filter from './components/filter/Filter'
-import Header from './components/header/Header'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Card from "./components/card/Card";
+import Filter from "./components/filter/Filter";
+import Header from "./components/header/Header";
 
 function App() {
- 
+  const getInitialTheme = () => {
+    return localStorage.getItem("theme") || "dark";
+  };
+  const [theme, setTheme] = useState(getInitialTheme);
+
+  useEffect(() => {
+    document.body.className = theme + "-mode";
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+  };
 
   return (
     <>
-      <Header/>
-      <Filter/>
-      <Card/>
+      <Header theme={theme} toggleTheme={toggleTheme} />
+      <Filter />
+      <Card />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
-import './Header.css'
-const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
 
-export default Header
+import "./Header.css";
+
+type HeaderProps = {
+  theme: string;
+  toggleTheme: () => void;
+};
+
+const Header = ({ theme, toggleTheme }: HeaderProps) => {
+
+  return (
+    <div className={`header ${theme}-mode`}>
+      <img src={"./assets/images/logo.svg"} alt="logo" className="logo"/>
+      <button onClick={toggleTheme} className={`switch ${theme}-mode`}>
+        <img
+          src={
+            theme === "dark"
+              ? `./assets/images/icon-sun.svg`
+              : `./assets/images/icon-moon.svg`
+          }
+          alt="dark/light mode switch"
+          
+        />
+      </button>
+    </div>
+  );
+};
+
+export default Header;
