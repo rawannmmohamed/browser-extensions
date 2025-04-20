1920b4e4a0e838d8160714d3273cd34abd6b86a1
rawanelkhodary <relkhodary@smart.sa>
2025-04-21 01:39:59 +0200

    `updated Card component to display extensions in a grid layout and added dynamic filtering and data loading`

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,27 +1,54 @@
+import { useEffect, useState } from "react";
 import "./Card.css";
 
+type Extension = {
+  logo: string;
+  name: string;
+  description: string;
+  isActive: boolean;
+};
+
 const Card = ({ filter, theme }: { filter: string; theme: string }) => {
+  
+  const [extensions, setExtensions] = useState<Extension[]>([]);
+
+  useEffect(() => {
+    fetch("./data.json")
+      .then((res) => res.json())
+      .then((data) => setExtensions(data))
+      .catch((err) => console.error("Failed to load extensions:", err));
+  }, []);
+
+  const filteredExtensions = extensions.filter((ext) => {
+    if (filter === "all") return true;
+    if (filter === "active") return ext.isActive;
+    if (filter === "inactive") return !ext.isActive;
+    return true;
+  });
+
   return (
     <div className="cards">
-      <div className={`card ${theme}`}>
-        <div className="extension">
-          <img
-            src="./assets/images/logo-devlens.svg"
-            alt="extension logo"
-            className="extension-logo"
-          />
-          <div>
-            <div className={`name ${theme}`}>DevLens</div>
-            <div className={`description ${theme}`}>
-              Quickly inspect page layouts and visualize element boundaries.
+      {filteredExtensions.map((extension, index) => (
+        <div key={index} className={`card ${theme}`}>
+          <div className="extension">
+            <img
+              src={extension.logo}
+              alt={`${extension.name} logo`}
+              className="extension-logo"
+            />
+            <div>
+              <div className={`name ${theme}`}>{extension.name}</div>
+              <div className={`description ${theme}`}>
+                {extension.description}
+              </div>
             </div>
           </div>
+          <div className="controls">
+            <button className={`remove ${theme}`}>Remove</button>
+            <button className="switch-mode">switch</button>
+          </div>
         </div>
-        <div className="controls">
-          <button className={`remove ${theme}`}>Remove</button>
-          <button className="switch-mode">switch</button>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
